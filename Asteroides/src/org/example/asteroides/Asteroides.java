9a8ccc1db6bd9be3018bedd22b043cd2322fe03c
pmjmadrid <pmjformacion@gmail.com>
2013-02-19 21:36:44 +0100

    Mom's House End of Module 2

--- Asteroides/src/org/example/asteroides/Asteroides.java
+++ Asteroides/src/org/example/asteroides/Asteroides.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Localizacion extends Activity {
+public class Asteroides extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
