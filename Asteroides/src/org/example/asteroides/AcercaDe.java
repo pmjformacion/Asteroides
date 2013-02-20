1a10fa8463ec7aeb24f6bed2d393c346cf8f5e72
pmjmadrid <pmjformacion@gmail.com>
2013-02-20 13:57:06 +0100

    MomHouse - Modulo 3. Añadí Activicad "Acerca De" de la primera manera

---
+++ Asteroides/src/org/example/asteroides/AcercaDe.java
@@ -0,0 +1,19 @@
+package org.example.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class AcercaDe extends Activity {
+
+	/**
+	 * Called when the actvity in first created
+	 */
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acercade);
+	}
+	
+}
